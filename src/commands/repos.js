7528e415be96967f2a5ce49d532dd676d0cbaa63
Dan Moore <github@mooreds.com>
2019-11-18 08:28:46 -0700

    hardcode repos command

--- src/commands/repos.js
+++ src/commands/repos.js
@@ -8,10 +8,12 @@ const trending = require('github-trending')
 const msgDefaults = {
   response_type: 'in_channel',
   username: 'Starbot',
-  icon_emoji: config('ICON_EMOJI')
+  icon_emoji: config('ICON_EMOJI'),
+  text: 'wah-wah'
 }
 
 const handler = (payload, res) => {
+/*
   trending('javascript', (err, repos) => {
     if (err) throw err
 
@@ -28,11 +30,11 @@ const handler = (payload, res) => {
       channel: payload.channel_name,
       attachments: attachments
     }, msgDefaults)
-
+*/
     res.set('content-type', 'application/json')
     res.status(200).json(msg)
     return
-  })
+  //})
 }
 
 module.exports = { pattern: /repos/ig, handler: handler }
